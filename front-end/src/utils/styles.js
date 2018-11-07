bd74d9ef8fb3e7c4798c25fb2aa702bc32aefcf7
Wilhelm Wanecek <wilhelm.wanecek@gmail.com>
2018-11-07 14:17:46 +0100

    Remove redundant media-query
    Since one of the two queries will always be true,
    there's no point in having both within @media blocks.
    Arguably it's easier to read with a "default" case and
    a special-case

--- front-end/src/utils/styles.js
+++ front-end/src/utils/styles.js
@@ -8,14 +8,12 @@ export const AppContainer = styled.div`
     '. header .'
     '. . .'
     ' . numbers . ';
+  grid-template-rows: 30px 90px 30px 1fr;
+  grid-template-columns: 50px 1fr 50px;
 
   @media (max-width: 700px) {
     grid-template-rows: 30px 90px 10px 1fr;
     grid-template-columns: 20px 1fr 20px;
-  }
-  @media (min-width: 700px) {
-    grid-template-rows: 30px 90px 30px 1fr;
-    grid-template-columns: 50px 1fr 50px;
   }
 `
 
@@ -24,11 +22,9 @@ export const StyledHeader = styled.div`
   display: flex;
   justify-content: center;
   text-decoration: underline;
+  font-size: 3em;
 
   @media (max-width: 700px) {
     font-size: 1.7em;
-  }
-  @media (min-width: 700px) {
-    font-size: 3em;
   }
 `
