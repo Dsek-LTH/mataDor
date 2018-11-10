2465632efd47d98541c2f1e50d2cabc89e5b8aa2
Wilhelm Wanecek <wilhelm.wanecek@gmail.com>
2018-11-10 01:37:50 +0100

    Use KeyboardEvent.key instead of keyCode
    This because keyCode is officially deprecated (it's a mess,
    key vs code vs which vs keyCode etc)

--- front-end/src/AdminBar/AdminView.js
+++ front-end/src/AdminBar/AdminView.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { addOrRemove, clear } from "../utils/api";
 import { AdminPanel, FocusInput, ColoredButton } from "./adminStyles";
-import {
-  ESCAPE_KEY,
-  ENTER_KEY,
-  NUMPAD_ENTER_KEY,
-  BACKSPACE,
-  DELETE
-} from "../utils/keypadNumbers";
 
 class AdminView extends React.Component {
   constructor(props) {
@@ -16,24 +9,22 @@ class AdminView extends React.Component {
   }
 
   // Implemented this way to always accept input!
-  handleKeyDown = event => {
-    const keyCode = event.keyCode;
-    switch (keyCode) {
-      case ESCAPE_KEY:
+  handleKeyDown = ({ key }) => {
+    switch (key) {
+      case "Escape":
         this.setState({ typed: "" });
         break;
-      case ENTER_KEY:
-      case NUMPAD_ENTER_KEY:
+      case "Enter":
         this.sendNumber();
         break;
-      case DELETE:
-      case BACKSPACE:
+      case "Del":
+      case "Backspace":
         this.setState({
           typed: this.state.typed.substring(0, this.state.typed.length - 1)
         });
         break;
       default:
-        this.handleIfNumber(keyCode);
+        this.handleIfNumber(key);
     }
   };
 
@@ -78,7 +69,7 @@ class AdminView extends React.Component {
         <ColoredButton color="#dbafc1" area="clear" onClick={clear}>
           rensa
         </ColoredButton>
-        <FocusInput value={this.state.typed} type="number" />
+        <FocusInput value={this.state.typed} type="number"/>
         <ColoredButton color="#b4d2ba" area="undo" onClick={this.undo}>
           :)
         </ColoredButton>
