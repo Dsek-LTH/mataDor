b647c8c917abf42ba5ff830704c54feda453b8a3
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-11-13 10:40:51 +0100

    Now "uses" query param as well

--- front-end/src/Routes/HomeRoute.js
+++ front-end/src/Routes/HomeRoute.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Redirect } from "react-router";
 import Header from "../utils/Header";
 import NumberContainer from "../Numbers/NumberContainer";
 import NumberListFetcher from "../Numbers/NumberListFetcher";
 import NotifyMe from "../utils/NotifyMe";
 import { AppContainer } from "../utils/styles";
 
-export default () => (
+export default ({ location }) => (
   <AppContainer>
+    {location.search.includes("tv") && (
+      <Redirect to="/tv" />
+    ) /*Any url with TV in it redirects to tv*/}
     <NumberListFetcher
       render={({ numberList }) => [
         <Header text="Mat redo för upphämtning" />,
