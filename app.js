c25cd9d23bf2ad4be327189402ebb0c1276fcaac
Wilhelm Wanecek <wilhelm.wanecek@gmail.com>
2018-12-15 13:11:06 +0100

    Comment and minor simplifications in app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const EventEmitter = require("events");
-const bodyParser = require("body-parser");
-const path = require("path");
+const express = require('express');
+const EventEmitter = require('events');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const config = require("./config");
 
@@ -12,58 +12,59 @@ app.use(bodyParser.json());
 
 // We don't use a real database, because we don't care if the server crashes,
 // and we don't want to store any information more than 1 pub.
-let currentFood = [];
+let currentFood = []; // string[]
 
-app.get("/subscribe", (req, res) => {
+app.get('/subscribe', (req, res) => {
   // Setup an event stream
   res.writeHead(200, {
-    Connection: "keep-alive",
-    "Content-Type": "text/event-stream",
-    "Cache-Control": "no-cache"
+    Connection: 'keep-alive',
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
   });
 
   function onPush(event, data) {
     res.write(`data: ${data} \n\n`);
   }
 
-  Stream.on("push", onPush);
+  Stream.on('push', onPush);
 
-  req.on("close", function() {
-    Stream.removeListener("push", onPush);
+  req.on('close', function() {
+    Stream.removeListener('push', onPush);
   });
 
-  // Give list to new ppl
+  // Give list to new clients
   sendList();
 });
 
-app.post("/addOrRemove", (req, res) => {
+app.post('/addOrRemove', (req, res) => {
   const id = req.body.id.toString(); // we need this to check length
+  // isNaN ensures we only accept numerical values
   if (id.length < 9 && !isNaN(id)) {
     const operation = addOrRemoveFood(id);
     return res.json({ message: `food was ${operation}` });
   }
   res.status(400).json({
-    message: "FoodId must be 1-8 digit integer"
+    message: 'Bad Request: id must be 1-8 digit integer.',
   });
 });
 
-app.post("/clear", (req, res) => {
+app.post('/clear', (req, res) => {
   clearList();
-  res.json({ message: "foodlist was cleared" });
+  res.json({ message: 'foodlist was cleared' });
 });
 
 // Not doing real hosting to avoid CORS for now
-app.use(express.static(path.join(__dirname, "front-end/build")));
+app.use(express.static(path.join(__dirname, 'front-end/build')));
 // this makes react router handle 404s
-app.get("*", (req, res) =>
+app.get('*', (req, res) =>
   res.sendFile(path.join(`${__dirname}/front-end/build/index.html`))
 );
 
-app.listen(config.port, () => console.log("\nApp started at " + config.port));
+app.listen(config.port, () => console.log("\nApp started at http://localhost:" + config.port));
 
 // Helpers that should be in another file
 const sendList = () =>
-  Stream.emit("push", "list", JSON.stringify(getCurrentList()));
+  Stream.emit('push', 'list', JSON.stringify(getCurrentList()));
 
 const getCurrentList = () => currentFood;
 
@@ -72,15 +73,15 @@ const clearList = () => {
   sendList();
 };
 
-const isInList = num => currentFood.filter(item => item === num).length !== 0;
+const isInList = num => currentFood.indexOf(num) > -1;
 
 const addOrRemoveFood = num => {
   if (!isInList(num)) {
-    currentFood = currentFood.concat(num);
+    currentFood.push(num);
     sendList();
-    return "added";
+    return 'added';
   }
   currentFood = currentFood.filter(item => item !== num);
   sendList();
-  return "removed";
+  return 'removed';
 };
